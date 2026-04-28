9ae551a18df8e2c0ce22e664d645e62d60cb035b
igaramadana <igrmdns085@gmail.com>
2026-04-28 22:31:29 +0700

    feat: add constants for user roles and application routes

---
+++ constants/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  LOGIN: "/login",
+
+  ADMIN_DASHBOARD: "/admin/dashboard",
+  ADMIN_KELUARGA: "/admin/keluarga",
+  ADMIN_KRITERIA: "/admin/kriteria",
+  ADMIN_AHP: "/admin/ahp",
+  ADMIN_SAW: "/admin/saw",
+  ADMIN_IMPORT: "/admin/import",
+
+  USER_DASHBOARD: "/user/dashboard",
+  USER_STATUS: "/user/status",
+} as const;
