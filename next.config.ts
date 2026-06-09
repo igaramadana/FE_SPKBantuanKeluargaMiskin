c4bb163c05eac3ae70fd664da2a754a6f8851831
igaramadana <igrmdns085@gmail.com>
2026-06-09 17:49:33 +0700

    feat: add rewrites configuration for backend API routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend-api/:path*",
+        destination: "http://160.187.141.236:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
